2f6d446bcc877263395ae5b52555a4f8cc4ffb73
Rajendrankr25 <rajendrankr25@gmail.com>
2022-11-17 16:43:19 +0530

    react w1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
 
+//component declaration
 function App() {
+  const data = [
+    {
+      name: "Raja",
+      pic: "https://pbs.twimg.com/media/EjY_lxhUcAAqCde?format=jpg&name=large"
+    },
+    {
+      name: "Selva",
+      pic: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202207/Ajith_departmental_store_Londo_1200x768.jpeg?VersionId=8RezllE_ALcqY2w70ciBep0Y1cBpoV0l&size=690:388"
+    },
+    {
+      name: "Dhiya",
+      pic: "https://static.scientificamerican.com/sciam/cache/file/ACB55B71-2661-471A-A45241D0CC7CD181_source.jpg"
+    },
+    {
+      name: "Aara",
+      pic: "https://static.scientificamerican.com/sciam/cache/file/ACB55B71-2661-471A-A45241D0CC7CD181_source.jpg"
+    }]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello All</h1>
+      <h2>This is Rajendran Ramasamy!!</h2>
+      {/* <h3>this is session workings</h3> */}
+      {/* <Test1
+        name="Raja"
+        pic="https://pbs.twimg.com/media/EjY_lxhUcAAqCde?format=jpg&name=large" />
+      <Test1
+        name="Selva"
+        pic="https://akm-img-a-in.tosshub.com/indiatoday/images/story/202207/Ajith_departmental_store_Londo_1200x768.jpeg?VersionId=8RezllE_ALcqY2w70ciBep0Y1cBpoV0l&size=690:388" />
+      <Test1
+        name="Dhiya"
+        pic="https://static.scientificamerican.com/sciam/cache/file/ACB55B71-2661-471A-A45241D0CC7CD181_source.jpg" /> */}
+      {data.map((a) => (
+        <Test1 name={a.name} pic={a.pic} />
+      ))}
     </div>
+  );
+}
+//custom component
+function Test1({ pic, name }) { //we can use object destructring here
+  //const { pic, name } = props; //object destructuring feature in ES6
+  return (  //inside return is a JSX code and it will be converted to javascript by webpack+babel
+    <>
+      <img className="img"
+        src={pic} alt={name} />
+      <h1 className='test'>Hello <span className="nameStyle">{name}</span> ❤😍</h1>
+    </>
   );
 }
 
